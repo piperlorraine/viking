32cd0dc94b37adf0f16ca7f5e251f9285bfea6c2
Piper Semerau <piperlorraine@gmail.com>
2013-02-19 14:17:22 -0800

    client changes to title and search bar

--- js/custom.js
+++ js/custom.js
@@ -1,2 +1,9 @@
 $(document).ready(function() {
+	$("#ctl00_header_txtSearch").click(function() {
+		if($(this).val() == "SEARCH") {
+			$(this).val("");
+		} else if($(this).val() == "") {
+			$(this).val() = "SEARCH";
+		}
+	});
 });
